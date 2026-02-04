e7900a56a3c443d8cb63ef4272fad5818aff2d8b
LAB13_USER ON WS33 <L13X16W33@LabHCI.net>
2026-02-04 17:01:48 +0800

    added backend

---
+++ backend/Relacion_lab1/Relacion_lab1/src/main/java/com/example/Relacion_lab1/RelacionLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.Relacion_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionLab1Application.class, args);
+	}
+
+}
